8a44bad520e60d511010e9ee7ecee385cfedbcaa
vladryanka <vsmorzhok@mail.ru>
2025-03-15 13:37:50 +0300

    Injection in Worker

--- app/src/main/java/com/example/cryptoapp/CoinApp.kt
+++ app/src/main/java/com/example/cryptoapp/CoinApp.kt
@@ -1,10 +1,25 @@
 package com.example.cryptoapp
 
 import android.app.Application
+import androidx.work.Configuration
+import com.example.cryptoapp.data.database.AppDatabase
+import com.example.cryptoapp.data.mapper.CoinMapper
+import com.example.cryptoapp.data.network.ApiFactory
+import com.example.cryptoapp.data.worker.RefreshDataWorkerFactory
 import com.example.cryptoapp.di.DaggerApplicationComponent
 
-class CoinApp: Application() {
+class CoinApp : Application(), Configuration.Provider {
     val component by lazy {
         DaggerApplicationComponent.factory().create(this)
     }
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(
+                RefreshDataWorkerFactory(
+                    AppDatabase.getInstance(this).coinPriceInfoDao(),
+                    ApiFactory.apiService,
+                    CoinMapper()
+                )
+            )
+            .build()
 }
--- app/src/main/java/com/example/cryptoapp/di/ViewModelModule.kt
+++ app/src/main/java/com/example/cryptoapp/di/ViewModelModule.kt
@@ -2,7 +2,6 @@ package com.example.cryptoapp.di
 
 import androidx.lifecycle.ViewModel
 import com.example.cryptoapp.presentation.CoinViewModel
-import com.example.cryptoapp.presentation.ViewModelFactory
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -13,5 +12,5 @@ interface ViewModelModule {
     @Binds
     @IntoMap
     @ViewModelKey(CoinViewModel::class)
-    fun bindViewModel(viewModel: ViewModelFactory): ViewModel
+    fun bindCoinViewModel(viewModel: CoinViewModel): ViewModel
 }
