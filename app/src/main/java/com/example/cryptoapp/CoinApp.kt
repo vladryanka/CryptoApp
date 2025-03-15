042f48d58d852f6bb5438fe478b6ffd033b9a93e
vladryanka <vsmorzhok@mail.ru>
2025-03-15 13:37:50 +0300

    Injection in Worker

--- app/src/main/java/com/example/cryptoapp/CoinApp.kt
+++ app/src/main/java/com/example/cryptoapp/CoinApp.kt
@@ -1,10 +1,28 @@
 package com.example.cryptoapp
 
 import android.app.Application
+import androidx.work.Configuration
+import com.example.cryptoapp.data.worker.RefreshDataWorkerFactory
 import com.example.cryptoapp.di.DaggerApplicationComponent
+import javax.inject.Inject
 
-class CoinApp: Application() {
+
+class CoinApp : Application(), Configuration.Provider {
+
+    override fun onCreate() {
+        component.inject(this)
+        super.onCreate()
+    }
+
+    @Inject
+    lateinit var workerFactory: RefreshDataWorkerFactory
     val component by lazy {
         DaggerApplicationComponent.factory().create(this)
     }
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(
+                workerFactory
+            )
+            .build()
 }
--- app/src/main/java/com/example/cryptoapp/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/cryptoapp/di/ApplicationComponent.kt
@@ -1,16 +1,20 @@
 package com.example.cryptoapp.di
 
 import android.app.Application
+import com.example.cryptoapp.CoinApp
 import com.example.cryptoapp.presentation.CoinDetailFragment
 import com.example.cryptoapp.presentation.CoinPriceListActivity
 import dagger.BindsInstance
 import dagger.Component
 
 @Component(modules = [DataModule::class, ViewModelModule::class])
+@ApplicationScope
 interface ApplicationComponent {
     fun inject(activity: CoinPriceListActivity)
 
     fun inject(fragment: CoinDetailFragment)
+
+    fun inject(application: CoinApp)
 
     @Component.Factory
     interface Factory {
